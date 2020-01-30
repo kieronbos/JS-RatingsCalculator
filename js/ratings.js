ef5cf1e53f97b3a118ea9086be815e01a7e3773c
Boswell <Kieron.Boswell@pspsl.co.uk>
2020-01-30 13:49:14 +0000

    event listener done

--- js/ratings.js
+++ js/ratings.js
@@ -6,6 +6,18 @@ const collect_ratings = () => {
     const elements = document.querySelectorAll('.rating');
     elements.forEach(element => {
         rating = parseInt(element.id.replace('star', ''));
-        parseInt(element.value);
+        ratings.count += parseInt(element.value);
+        ratings.sum += parseInt(element.value) * rating;
     });
+
+    if (ratings.count !== 0) {
+        ratings.average = ratings.sum / ratings.count;
+    }
+
+    return ratings;
 }
+
+document.addEventListener('change', () => {
+    const ratings = collect_ratings();
+    document.querySelector('#average').value = ratings.average.toFixed(2);
+})
