29d5eac106a378512ec3c2e186c36385a0992a00
Boswell <Kieron.Boswell@pspsl.co.uk>
2020-01-30 13:41:40 +0000

    ratings setup

--- js/ratings.js
+++ js/ratings.js
@@ -1,3 +1,11 @@
-function collect_ratings() {
+const collect_ratings = () => {
+    const ratings = { 'count': 0, 'sum': 0, 'average': 0 };
 
+    let rating = 0;
+
+    const elements = document.querySelectorAll('.rating');
+    elements.forEach(element => {
+        rating = parseInt(element.id.replace('star', ''));
+        parseInt(element.value);
+    });
 }
